009e183a18c167a7d941c905ba0c81f517fae883
bertliang <bert_liang@yahoo.ca>
2015-04-09 23:02:33 -0400

    use mongolab db

--- config.js
+++ config.js
@@ -10,6 +10,8 @@ sessionTimeout: 1000*60*2,  // 2 minute session timeout
 
 env:'development',
 
-db: 'mongodb://127.0.0.1/test'
+db: 'mongodb://a:a@ds029811.mongolab.com:29811/berttest'
+
+//db: 'mongodb://127.0.0.1/test'
 
 };
